ac40cd011d8f4bad0d709a01f78b0bdca4ed0476
aldi.arpateam <aldi1900018037@webmail.uad.ac.id>
2022-09-17 15:56:13 +0700

    7 - 15 Oktober 2022 (Membuat Menu & Header Website)

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Wakaf Pembebasan Lahan Untuk Rumah Tahfidz Qur'an</title>
+    <meta content="Yuk Berwakaf Dengan Membantu Yayasan GCK Foundation Tasikmalaya Butuh Bantuan Pembebasan Lahan Untuk Rumah Tahfidz Qur'an" name="description">
+    <meta content="Yuk Berwakaf Dengan Membantu Yayasan GCK Foundation Tasikmalaya Butuh Bantuan Pembebasan Lahan Untuk Rumah Tahfidz Qur'an" name="keywords">
+    <!-- Favicons -->
+    <link href="assets/img/icon-gck-foundation.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+         
+    <meta name="googlebot" content="index,follow">
+    <meta name="googlebot-news" content="index,follow">
+    <meta name="robots" content="index,follow">
+    <meta name="Slurp" content="all">
+
+    <meta property="fb:app_id" content="501046580289991">
+    <meta property="og:title" content="Wakaf Pembebasan Lahan Untuk Rumah Tahfidz Qur'an">
+    <meta property="og:type" content="article">
+    <meta property="og:site_name" content="Yuk Berwakaf Dengan Membantu Yayasan GCK Foundation Tasikmalaya Butuh Bantuan Pembebasan Lahan Untuk Rumah Tahfidz Qur'an">
+  
+    <meta name="image_src" content="assets/img/banner-gck-foundation-wakaf.png">
+    <meta property="og:image" content="assets/img/banner-gck-foundation-wakaf.png">
+    <meta property="og:image:alt" content="assets/img/banner-gck-foundation-wakaf.png">
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="620" />
+    <meta property="og:image:height" content="413" />
+    <meta property="og:url" content="https://www.wakaf.gckfoundation.or.id/">
+    
+    <link rel="canonical" href="https://www.wakaf.gckfoundation.or.id/">
+    
+    <meta property="og:description" content="Yuk Berwakaf Dengan Membantu Yayasan GCK Foundation Tasikmalaya Butuh Bantuan Pembebasan Lahan Untuk Rumah Tahfidz Qur'an">
+    <meta name="news_keywords" content="Yuk Berwakaf Dengan Membantu Yayasan GCK Foundation Tasikmalaya Butuh Bantuan Pembebasan Lahan Untuk Rumah Tahfidz Qur'an">
+    <link rel="shortlink" href="www.wakaf.gckfoundation.or.id">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+        <div class="container d-flex align-items-center justify-content-between">
+            <div class="logo">
+                 <a href="https://www.wakaf.gckfoundation.or.id/"><img src="assets/img/logo-gck-foundation.png" alt="Logo GCK Foundation" class="img-fluid"></a>
+            </div>
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li>
+                        <a class="nav-link scrollto active" href="#hero">Beranda</a>
+                    </li>
+                    <li>
+                        <a class="nav-link scrollto" href="#details">Tentang Wakaf</a>
+                    </li>
+                    <li>
+                        <a class="nav-link scrollto" href="#gallery">Detail Objek Wakaf</a>
+                    </li>
+                    <li>
+                        <a class="nav-link scrollto" href="#pricing">Yuk Wakaf</a>
+                    </li>
+                    <li>
+                        <a class="nav-link scrollto" href="#rekening"><span class="btn btn-warning">No. Rekening</span></a>
+                    </li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+            <!-- .navbar -->
+        </div>
+    </header>
+    <!-- End Header -->
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-lg-7 pt-5 pt-lg-0 order-2 order-lg-1 text-center text-lg-start d-flex align-items-center">
+                    <div data-aos="zoom-out">
+                        <h1 class="text-uppercase">Wakaf Pembebasan Lahan <span>Rumah Tahfidz Qur'an</span>
+                        </h1>
+                        <h2 class="mt-4"><span>Lokasi:</span> Kp. Sukamulya Ds. Sindangraja Kec. Jamanis. Kab. Tasikmalaya Prov. Jawa Barat</h2>
+                        <div class="text-center text-lg-start">
+                            <a href="#rekening" class="btn-get-started scrollto">BRI 4442-01-016045-53-1</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5 order-1 order-lg-2 d-block d-md-none text-center" data-aos="zoom-out" data-aos-delay="300">
+                    <img src="assets/img/banner-gck-foundation-wakaf.png" class="img-fluid w-75 animated" alt="Gambar Banner GCK Foundation">
+                </div>
+                <div class="col-lg-4 order-1 order-lg-2 hero-img d-none d-md-block" data-aos="zoom-out" data-aos-delay="300">
+                    <img src="assets/img/banner-gck-foundation-wakaf.png" class="img-fluid animated" alt="Gambar Banner GCK Foundation">
+                </div>
+            </div>
+        </div>
+        <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
+            <defs>
+                <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
+            </defs>
+            <g class="wave1">
+                <use xlink:href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)">
+            </g>
+            <g class="wave2">
+                <use xlink:href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)">
+            </g>
+            <g class="wave3">
+                <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
+            </g>
+        </svg>
+    </section>
+    <div id="preloader"></div>
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    <script>
+        function copyRekening() {
+            /* Get the text field */
+            var copyText = document.getElementById("myRekening");
+
+            /* Select the text field */
+            copyText.select();
+            copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+           /* Copy the text inside the text field */
+            navigator.clipboard.writeText(copyText.value);
+
+            /* Alert the copied text */
+            alert("Copied the text: " + copyText.value);
+        }
+    </script>
+</body>
+</html>
