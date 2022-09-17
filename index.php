214f2d16b609a1cf39428f512d7890cb98a1922a
aldi.arpateam <aldi1900018037@webmail.uad.ac.id>
2022-09-17 15:57:21 +0700

    17 - 29 Oktober 2022 (Membuat konten website)

--- index.php
+++ index.php
@@ -115,6 +115,251 @@
             </g>
         </svg>
     </section>
+    <!-- End Hero -->
+    <main id="main">
+        <!-- ======= Details Section ======= -->
+        <section id="details" class="details">
+            <div class="container">
+                <div class="row content">
+                    <div class="col-md-12 pt-5 order-2 order-md-1 text-center" data-aos="fade-up">
+                        <h1>Assalamu'alaikum Warohmatullahiwabarokatuh</h1>
+                        <h3>Meraih Keberkahan Dengan Berwakaf</h3>
+                        <p>Secara istilah <strong>wakaf</strong> diartikan sebagai suatu jenis pemberian yang dilakukan dengan cara menahan dan menjadikannya bermanfaat untuk kemaslahatan umat. Adapun yang dimaksud dengan menahan adalah menghindarkan barang tersebut agar tidak diwariskan, dijual, dihibahkan, digadaikan, disewakan, dan sejenisnya.</p>
+                        <p>Banyak pendapat mengatakan wakaf termasuk dalam <strong>sedekah jariyah</strong>. <strong>Sedekah</strong> artinya memberikan sesuatu tanpa paksaan. Sementara <strong>jariyah</strong> artinya mengalir (pahalanya).</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ======= Testimonials Section ======= -->
+        <section class="testimonials">
+            <div class="container">
+                <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="testimonial-item">
+                                <h3>Disebutkan dalam sebuah hadits riwayat Muslim bahwa ada tiga amalan yang tidak terputus walaupun telah meninggal dunia, salah satunya sedekah jariyah (wakaf).</h3>
+                                <h4>Dari Abu Hurairah ra. berkata: Rasulullah bersabda:</h4>
+                                <p>
+                                    <i class="bx bxs-quote-alt-left quote-icon-left"></i> Apabila manusia itu meninggal dunia maka terputuslah segala amalnya kecuali dari tiga sumber: yaitu sedekah jariyah, ilmu yang bermanfaat atau anak sholeh yang mendoakan kepadanya. (HR Muslim). <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                                </p>
+                            </div>
+                        </div>
+                        <!-- End testimonial item -->
+                        <div class="swiper-slide">
+                            <div class="testimonial-item">
+                                <h3>Dalam hadits lain dikatakan bahwa wakaf termasuk amalan yang akan diperoleh ketika sudah meninggal dunia. Hadits ini memperjelas pendapat sebelumnya tentang wakaf sebagai amal jariyah.</h3>
+                                <h4>Nabi Muhammad SAW Bersabda</h4>
+                                <p>
+                                    <i class="bx bxs-quote-alt-left quote-icon-left"></i> Sesungguhnya di antara amal shaleh yang mendatangkan pahala setelah orang yang mengamalkannya meninggal dunia, yaitu; Ilmu yang disebarluaskan olehnya, Anak shaleh yang ditinggalkannya, Mushaf (alquran) yang diwariskannya, Masjid yang dibangunnya, Rumah yang didirikan dengan tujuan dijadikan sebagai tempat bermalam (penginapan) orang yang sedang dalam perjalanan (ibnu sabil), Sungai yang dialirkan guna kepentingan orang banyak, Harta yang disedekahkannya. (HR. Ibnu Majah) <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                                </p>
+                            </div>
+                        </div>
+                        <!-- End testimonial item -->
+                    </div>
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
+        </section>
+        <!-- End Testimonials Section -->
+
+        <section class="details">
+            <div class="container">
+                <div class="row content">
+                    <div class="col-md-4 my-auto order-1 order-md-2" data-aos="fade-left">
+                        <img src="assets/img/wakaf-tahfidz-quran-1.jpeg" class="img-fluid rounded-10 shadow-sm mb-4 mb-md-0" alt="Gambar Wakaf memiliki banyak keutamaan di antaranya menanamkan sifat zuhud dan melatih diri untuk menolong kepentingan orang lain.">
+                    </div>
+                    <div class="col-md-8 my-auto order-2 order-md-1" data-aos="fade-up">
+                        <h1 class="h1-v2">Wakaf memiliki banyak keutamaan di antaranya menanamkan sifat zuhud dan melatih diri untuk menolong kepentingan orang lain.</h1>
+                        <h3>Begitu dahsyatnya amalan ini untuk kita laksanakan.</h3>
+                        <p class="my-4">Kami dari <strong>Yayasan Gapura Cahyaning Kahuripan</strong> mengajak kepada Ayah, Bunda dan Kaka untuk ikut dalam Program Wakaf, marilah ikut berpartisipasi dalam program pembebasan lahan & pembangunan gedung untuk <strong>RUMAH TAHFIDZ CAHAYA KAHURIPAN</strong>.</p>
+                        <h3>Pahala mengalir terus, meski usia sudah terputus.</h3>
+                    </div>
+                </div>
+                <div class="row content">
+                    <div class="col-md-4 my-auto" data-aos="fade-right">
+                        <img src="assets/img/wakaf-tahfidz-quran-2.jpeg" class="img-fluid rounded-10 shadow-sm mb-4 mb-md-0" alt="Gambar Kenapa Harus Wakaf Pembebasan Lahan Dan Pembangunan Gedung Rumah Tahfidz?">
+                    </div>
+                    <div class="col-md-8 my-auto" data-aos="fade-up">
+                        <h1 class="h1-v2">Kenapa Harus Wakaf Pembebasan Lahan Dan Pembangunan Gedung Rumah Tahfidz?</h1>
+                        <ul>
+                            <li>
+                                <i class="bi bi-check"></i> Pahala dari shalat tahajud terputus setelah shalat itu selesai.
+                            </li>
+                            <li>
+                                <i class="bi bi-check"></i> Pahala dari puasa sunah terputus setelah berbuka.
+                            </li>
+                            <li>
+                                <i class="bi bi-check"></i> Pahala dari baca Al-Quran terputus setelah berhenti baca Al-Quran.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row content">
+                    <div class="col-md-4 my-auto order-1 order-md-2" data-aos="fade-left">
+                        <img src="assets/img/wakaf-tahfidz-quran-3.jpeg" class="img-fluid rounded-10 shadow-sm mb-4 mb-md-0" alt="Gambar Tapi Tidak Dengan Pahala Wakaf Pembebasan Lahan Rumah Tahfidz Dan Pembangunan Gedung">
+                    </div>
+                    <div class="col-md-8 my-auto order-2 order-md-1" data-aos="fade-up">
+                        <h1 class="h1-v2">Tapi Tidak Dengan Pahala Wakaf Pembebasan Lahan Rumah Tahfidz Dan Pembangunan Gedung:</h1>
+                        <ul>
+                            <li>
+                                <i class="bi bi-check"></i> Pahalanya terus mengalir walau sedekahnya sudah selesai.
+                            </li>
+                            <li>
+                                <i class="bi bi-check"></i> Tidak terputus sekalipun pelakunya sudah meninggal dunia.
+                            </li>
+                            <li>
+                                <i class="bi bi-check"></i> Setiap civitas dan Santri yang mengaji, shalat dan beribadah di Rumah Tahfidz pahalanya mengalir kepada Anda, tanpa mengurangi pahala yang mengerjakannya.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Details Section -->
+        <!-- ======= Gallery Section ======= -->
+        <section id="gallery" class="gallery">
+            <div class="container">
+                <div class="section-title" data-aos="fade-up">
+                    <h2>Detail Objek Wakaf </h2>
+                    <p>Alamat : Kp. Sukamulya Ds. Sindangraja Kec. Jamanis. Kab. Tasikmalaya Prov. Jawa Barat</p>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-xl-5 my-auto" data-aos="fade-right">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.416798432777!2d108.18255331477454!3d-7.19319229480676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f4f6def92003b%3A0xb0f78ec9b21a4275!2sGCK%20Foundation%20Tasikmalaya!5e0!3m2!1sid!2sid!4v1649162552759!5m2!1sid!2sid" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="rounded-10"></iframe>
+                    </div>
+                    <div class="col-md-6 col-xl-7 my-auto mt-4 mt-md-0">
+                        <div class="row justify-content-center g-1" data-aos="fade-left">
+                            <div class="col-sm-6 col-xl-4">
+                                <div class="gallery-item rounded-10" data-aos="zoom-in" data-aos-delay="100">
+                                    <a href="assets/img/foto-wakaf/gambar-lokasi-wakaf-4-63.jpeg" class="gallery-lightbox">
+                                        <img src="assets/img/foto-wakaf/gambar-lokasi-wakaf-4-63.jpeg" alt="gambar-lokasi-wakaf-4-63.jpeg" class="img-fluid">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-xl-4">
+                                <div class="gallery-item rounded-10" data-aos="zoom-in" data-aos-delay="100">
+                                    <a href="assets/img/foto-wakaf/gambar-lokasi-wakaf-1-15.jpeg" class="gallery-lightbox">
+                                        <img src="assets/img/foto-wakaf/gambar-lokasi-wakaf-1-15.jpeg" alt="gambar-lokasi-wakaf-1-15.jpeg" class="img-fluid">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-xl-4">
+                                <div class="gallery-item rounded-10" data-aos="zoom-in" data-aos-delay="100">
+                                    <a href="assets/img/foto-wakaf/gambar-lokasi-wakaf-2-59.jpeg" class="gallery-lightbox">
+                                        <img src="assets/img/foto-wakaf/gambar-lokasi-wakaf-2-59.jpeg" alt="gambar-lokasi-wakaf-2-59.jpeg" class="img-fluid">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-xl-4">
+                                <div class="gallery-item rounded-10" data-aos="zoom-in" data-aos-delay="100">
+                                    <a href="assets/img/foto-wakaf/gck-foundation-selamat-tahun-baru-1443-h-75.jpeg" class="gallery-lightbox">
+                                        <img src="assets/img/foto-wakaf/gck-foundation-selamat-tahun-baru-1443-h-75.jpeg" alt="gck-foundation-selamat-tahun-baru-1443-h-75.jpeg" class="img-fluid">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-xl-4">
+                                <div class="gallery-item rounded-10" data-aos="zoom-in" data-aos-delay="100">
+                                    <a href="assets/img/foto-wakaf/gambar-lokasi-wakaf-3-74.jpeg" class="gallery-lightbox">
+                                        <img src="assets/img/foto-wakaf/gambar-lokasi-wakaf-3-74.jpeg" alt="gambar-lokasi-wakaf-3-74.jpeg" class="img-fluid">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-xl-4">
+                                <div class="gallery-item rounded-10" data-aos="zoom-in" data-aos-delay="100">
+                                    <a href="assets/img/foto-wakaf/gck-foundation-pembinaan-olahraga-dan-kesehatan--96.jpeg" class="gallery-lightbox">
+                                        <img src="assets/img/foto-wakaf/gck-foundation-pembinaan-olahraga-dan-kesehatan--96.jpeg" alt="gck-foundation-pembinaan-olahraga-dan-kesehatan--96.jpeg" class="img-fluid">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-xl-4">
+                                <div class="gallery-item rounded-10" data-aos="zoom-in" data-aos-delay="100">
+                                    <a href="assets/img/foto-wakaf/gck-foundation-selamat-tahun-baru-1443-h-75.jpeg" class="gallery-lightbox">
+                                        <img src="assets/img/foto-wakaf/gck-foundation-selamat-tahun-baru-1443-h-75.jpeg" alt="gck-foundation-selamat-tahun-baru-1443-h-75.jpeg" class="img-fluid">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Gallery Section -->
+
+        <!-- ======= Pricing Section ======= -->
+        <section id="pricing" class="pricing section-bg">
+            <div class="container">
+                <div class="section-title" data-aos="fade-up">
+                    <h2>Yuk Wakaf</h2>
+                    <p>Detail Banguan dan Wakaf</p>
+                </div>
+
+                <div class="row mb-5" data-aos="fade-left">
+                    <div class="col-md-6 my-2">
+                        <div class="icon-box" data-aos="zoom-in" data-aos-delay="50">
+                            <i class="ri-community-line" style="color: #FF7300;"></i>
+                            <p>Luas Tanah & Bangunan</p>
+                            <h3>210 m²</h3>
+                        </div>
+                    </div>
+                    <div class="col-md-6 my-2">
+                        <div class="icon-box" data-aos="zoom-in" data-aos-delay="100">
+                            <i class="ri-bar-chart-box-line" style="color: #FF7300;"></i>
+                            <p>Harga Tanah dan Bangunan</p>
+                            <h3>Rp. 650.000.000,- atau Rp. 3.100.000,-/ m²</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row" data-aos="fade-left">
+                    <div class="col-md-6 mt-4 mt-md-0">
+                        <div class="box featured" data-aos="zoom-in" data-aos-delay="200">
+                            <h4 class="mb-4">Rp.3.100.000,- /m²</h4>
+                            <p>Untuk memudahkan pewakif (orang yang berwakaf) dapat berpartisipasi sebesar <strong>Rp.3.100.000,- /m²</strong> atau kelipatanya atau dibawah angka tersebut dipersilahkan tidak ada batasan wakaf tunai.</p>
+                            <p>Sertifikat wakaf Tunai diterbitkan dalam nominal <strong>Rp.3.100.000,- (1 m²)</strong> dan jumlah lain yang lebih besar.</p>
+                            <div class="btn-wrap">
+                                <a href="#rekening" class="btn-buy">Donasi Sekarang</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 mt-4 mt-md-0">
+                        <div class="box featured" data-aos="zoom-in" data-aos-delay="200">
+                            <h3 class="text-center">Wakaf Pembelian Lahan Juga Dapat Dicicil </h3>
+                            <h4>Rp.500.000,- (6x)</h4>
+                            <span>*cicilan terakhir Rp.600.000,-</span>
+
+                            <h4 class="mt-4">Rp.1.000.000,- (3x)</h4>
+                            <span>*cicilan terakhir Rp.1.100.000,-</span>
+                            <div class="btn-wrap">
+                                <a href="#rekening" class="btn-buy">Donasi Sekarang</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Pricing Section -->
+
+
+        <section id="rekening" class="rekening">
+            <div class="container">
+                <div class="card warning-bg-card text-center p-4" data-aos="fade-up">
+                    <h1>Yuk Salurkan Wakaf terbaik melalui<br />Rekening Yayasan Gapura Cahyaning Kahuripan</h1>
+                    <h2 class="my-4">
+                        <img src="assets/img/bri.svg" class="mb-3 myBank" alt="Logo Bank BRI">
+                        <br />
+                        <!-- The text field -->
+                        <input type="text" value="4442-01-016045-53-1" id="myRekening" class="text-center">
+
+                        <!-- The button used to copy the text -->
+                        <button onclick="copyRekening()" id="myRekeningButton"><i class='bx bxs-copy-alt'></i></button>
+                    </h2>
+                    <h5>Semoga pahala Wakaf terus mengalir hingga Yaumil Akhir</h5>
+                    <h5>Semoga menjadi wasilah untuk rizki yang melimpah dan barokah dunia serta akhirat.</h5>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- End #main -->
     <div id="preloader"></div>
     <!-- Vendor JS Files -->
     <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
