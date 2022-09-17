f9bed993fa464d117a263489ae638432f3d7c6d1
aldi.arpateam <aldi1900018037@webmail.uad.ac.id>
2022-09-17 15:58:13 +0700

    1 - 12 November 2022 (Membuat Footer Website)

--- index.php
+++ index.php
@@ -360,6 +360,22 @@
         </section>
     </main>
     <!-- End #main -->
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="container">
+            <div class="copyright"> &copy; <?= date("Y") ?> GCK Foundation <strong><a target="_blank" href="https://gckfoundation.or.id/" title="Website GCK Foundation">GCK Foundation</a></strong>. All Rights Reserved</div>
+            <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bootslander-free-bootstrap-landing-page-template/ --> Website by <strong><a target="_blank" href="https://arpateam.com/" title="Website #ARPATEAM">#ARPATEAM</a></strong>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
+        <i class="bi bi-arrow-up-short"></i>
+    </a>
     <div id="preloader"></div>
     <!-- Vendor JS Files -->
     <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
